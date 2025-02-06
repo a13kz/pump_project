d99c7e910926bf8bf3e5491842d87bcca199b7b0
a13kz <nils.alexander.bohre@gmail.com>
2025-02-06 14:01:30 +0100

    commenting

--- pump_project.ino
+++ pump_project.ino
@@ -2,14 +2,16 @@
   File: pump_project.ino
   Author: Alexander Bohre
   Date: 2025-02-05
-  Description: script for a automatic plant system that brings light and water to plants automaticly.
+  Description: script for automatic plant system that brings light and water to plants automaticly.
 */
 
+// Init constants
+const int length = 10;
+
 //initialising global variables
-const int length = 10; 
 int lastVal[length];
 int oldVal;
-int threshold; // Threshold to prevent light flickering.
+int threshold;            // Threshold to prevent light flickering.
 bool finished = false;
 int pumpTime = 10000;
 
@@ -25,32 +27,30 @@ void setup() {
 }
 
 void loop() {
-
-  // Calling functions
   checkSoil();
   checkLight();
   delay(100);
 }
+
 /*
-  Caluclates the moisture of the soil with input from A0
+  Caluclates the moisture of soil with input from A0
   Parameters: Void
   Returns: HIGH/LOW voltage on pin 8
 */
-void checkSoil(){
-  Serial.println(analogRead(A0));
+void checkSoil() {
 
-  if(analogRead(A0) > 500 && !finished){
+  if (analogRead(A0) > 750 && !finished) {  // Read the value of soil moisture
+
+
+    //pin 8 HIGH for 10s
     digitalWrite(8, HIGH);
     delay(pumpTime);
     digitalWrite(8, LOW);
-    Serial.println("finished");
-    finished = true;
-  }
-  else if (analogRead(A0) < 400){
-    finished = false;
-  }
- 
 
+    finished = true;                        //prevent from activation of pin 8
+  } else if (analogRead(A0) < 650) {
+    finished = false;                       // Allow activation for pin 8
+  }
 }
 
 /*
@@ -58,15 +58,14 @@ void checkSoil(){
   Parameters: Void
   Returns: HIGH/LOW voltage on pin 12 and changes the global variable buffer if light is detected
 */
-void checkLight(){
+void checkLight() {
 
-  // Takes value from updateMean 
+  // Takes value from updateMean
 
-  if(updateMean() + threshold > 950){
+  if (updateMean() + threshold > 950) {
     digitalWrite(12, HIGH);
-    threshold = 100; 
-    }
-  else{
+    threshold = 100;
+  } else {
     digitalWrite(12, LOW);
     threshold = 0;
   }
@@ -77,12 +76,11 @@ void checkLight(){
   Parameters: void
   Returns: returns an average of "length" amounts of inputs from A1.  
 */
-
 int updateMean() {
 
   // shifting register that updates the array "lastVal"
   int sum = 0;
-  lastVal[length-1] = analogRead(A1);
+  lastVal[length - 1] = analogRead(A1);
   for (int i = 1; i < length; i++) {
     lastVal[i - 1] = lastVal[i];
   }
@@ -91,5 +89,5 @@ int updateMean() {
   for (int i = 0; i < length; i++) {
     sum += lastVal[i];
   }
-  return sum / length; // Calculates the average of sum
+  return sum / length;  // Calculates the average of sum
 }
